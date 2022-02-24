7ce201f053753c007493dfca1e2c576e02632cbf
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-02-24 13:26:31 +0200

    Creating MostPopular file and Adding it to the App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HeaderCompound from './Compounds/HeaderCompound'
+import MostPopular from './Compounds/MostPopularCompound';
 
 function App() {
   return (
     <>
       <HeaderCompound />
+      <MostPopular />
     </>
   );
 }
---
+++ src/Compounds/MostPopularCompound.js
@@ -0,0 +1,85 @@
+import React from "react";
+import styled from "styled-components"
+
+function MostPopular() {
+  return (
+    <>
+      <Header>
+        Most popular stories today
+      </Header>
+      <Wrapper>
+        <StoryWrapper>
+          <ImageLink href='#'><Image src='./img/news-imgs/1.jfif' /></ImageLink>
+          <Text href='#'>We tried a bunch of free Windows apps from the Microsoft Store — here are our favorites</Text>
+        </StoryWrapper>
+
+        <StoryWrapper>
+          <ImageLink href='#'><Image src='./img/news-imgs/16.jfif' /></ImageLink>
+          <Text href='#'>Your brain might be a quantum computer that hallucinates math</Text>
+        </StoryWrapper>
+
+        <StoryWrapper>
+          <ImageLink href='#'><Image src='./img/news-imgs/12.jfif' /></ImageLink>
+          <Text href='#'>Here’s why your car isn’t rolling on airless tires… yet</Text>
+        </StoryWrapper>
+
+        <StoryWrapper>
+          <ImageLink href='#'><Image src='./img/news-imgs/17.jfif' /></ImageLink>
+          <Text href='#'>You can select all your typed text by tapping thrice on iOS</Text>
+        </StoryWrapper>
+
+        <StoryWrapper>
+          <ImageLink href='#'><Image src='./img/news-imgs/13.jfif' /></ImageLink>
+          <Text href='#'>Brace yourselves, Trump has just launched his social network</Text>
+        </StoryWrapper>
+      </Wrapper>
+    </>
+  );
+}
+
+export const Header = styled.h3`
+  /* border: 3px solid blue; */
+  text-align: center;
+`
+
+export const Wrapper = styled.div`
+  /* border: 3px solid red; */
+  display: flex;
+  justify-content: space-around;
+  margin-top: 40px;
+  width: 100%;
+`
+
+export const StoryWrapper = styled.div`
+  /* border: 3px solid green; */
+  width: 236px;
+  padding-left: 10px;
+  padding-right: 10px;
+`
+
+export const ImageLink = styled.a`
+  
+`
+
+export const Image = styled.img`
+  /* border: 3px solid red; */
+  width: 100%;
+  padding-bottom: 10px;
+`
+
+export const Text = styled.a`
+  /* border: 3px solid red; */
+  width: 83%;
+  color: #000000;
+  font-size: 16px;
+  font-weight: 600;
+  text-decoration: none;
+
+  :hover {
+    text-decoration: underline;
+    text-decoration-color: #ebadd6;
+    text-decoration-thickness: 2px;
+  }
+`
+
+export default MostPopular;
